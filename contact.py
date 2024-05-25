7409bce968ba12b1234d9304a4607b9ecf4f16b5
Longheist <127388819+Longheist@users.noreply.github.com>
2024-05-25 13:33:17 +0530

    Add files via upload

---
+++ contact.py
@@ -0,0 +1,22 @@
+import streamlit as st  # pip install streamlit
+
+st.header(":mailbox: Get In Touch With Me!")
+
+contact_form = """
+<form action="https://formsubmit.co/longheist123@gmail.com" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here" required></textarea>
+     <button type="submit" class="button">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def main_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+main_css("main_css.css")
